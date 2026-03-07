74028bff2a010cd8212ec020063a50925ffa26c4
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-03-07 21:51:11 +0000

    operators

--- 4. operators/01_assignment_operators.js
+++
@@ -1,3 +0,0 @@
-// assigment operators are used to assign values to variables. The most common assignment operator is the equal sign (=), which assigns the value on the right to the variable on the left.
-// Here are some examples of assignment operators in JavaScript: 
-// +, -, *, %, --, ++, +=, -=, *=, /=, %=    
---
+++ 4.operators/01_assignment_operators.js
@@ -0,0 +1,11 @@
+// JS has four types of operators:
+// 1. Assignment operators
+// 2. Arithmetic operators
+// 3. Comparison operators
+// 4. Logical operators
+
+// ASSIGNMENT OPERATORS
+// They include the following: +=, -=, *=, /=, %=, **=, <<=, >>=, >>>=, &=, ^=, |=
+
+let x = 10;
+console.log(x); // 10
